2a1fe703c763c4373c779bfce7ab3df4b1efb504
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-09-07 13:06:01 +0800

    Update

---
+++ let_const_03.js
@@ -0,0 +1,16 @@
+//let, const
+//How to name a variable
+//letters, numbers, _ , $
+//variable name cannot start wuth numbers
+
+//let keyword
+let student = 'Sara';
+student = 'Kim';
+
+console.log(student)
+
+
+//const
+const person = 'John';
+// person = 'Sam';
+console.log(person);
