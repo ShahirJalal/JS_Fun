a2d25276fdeb710c02c74bb63341c12b40f33cee
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-09-08 11:12:45 +0800

    Update switch_13.js

--- switch_13.js
+++ switch_13.js
@@ -1,3 +1,15 @@
-const color = 'red';
+const color = 'green';
 
-switch()
+switch(color){
+    case 'red':
+        console.log('color is red');
+        break;
+    case 'green':
+        console.log('Color is green');
+        break;
+    case 'pink':
+        console.log('Color is pink');
+        break;
+        default:
+            console.log('no valid color');
+}
