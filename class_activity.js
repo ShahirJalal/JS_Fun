f3359a7fd2ba13650be24e6fbfed4ab996ac1edd
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-09-08 14:13:35 +0800

    Update

---
+++ class_activity.js
@@ -0,0 +1,10 @@
+//  create an array of colors (4 colors)
+//  use array methods to add a color to end of that array
+//  use array method to add to the begining of that array
+//  check the length of that array
+
+const colors = ['cyan', 'magenta', 'yellow', 'black'];
+colors.push('green');
+colors.unshift('gray');
+console.log(colors)
+console.log(colors.length);
