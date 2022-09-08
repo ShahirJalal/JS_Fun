c762aef0ed571c6dca48a8f906c9b9552388d2b4
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-09-08 12:02:50 +0800

    Update

---
+++ function_14.js
@@ -0,0 +1,45 @@
+// Function declaration
+
+// function greet(firstName, lastName) {
+//     if(typeof firstName === 'undefined') {firstName = 'John'}
+//     if(typeof lastName === 'undefined') {lastName = 'Doe'}
+
+//     console.log(`Welcome ${firstName} ${lastName}`)
+// }
+function greet(firstName = 'John', lastName = 'Doe') {
+
+    console.log(`Welcome ${firstName} ${lastName}`)
+}
+
+// Function declaration
+greet();
+
+// Function expression
+
+const square = function(x = 3) {
+    return x*x;
+};
+
+console.log(square(5));
+
+// Immediate invokable function expression
+(function (){
+    console.log('IIFE RAN...')
+})();
+
+(function (userName = 'John'){
+    console.log(`welcome ${userName}`)
+})();
+
+// Property method
+
+const todo = {
+    add : function() {
+        console.log('add to the list');
+    },
+    edit : function() {
+        consolelog('edit the list');
+    }
+}
+
+todo.add();
